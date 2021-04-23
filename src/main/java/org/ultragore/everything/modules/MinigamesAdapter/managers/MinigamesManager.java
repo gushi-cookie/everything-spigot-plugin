c36572c4d0cb0104bda06622745eabb7ed39eb1c
gushi-cookie <gushi-cookie@protonmail.com>
2021-04-23 13:27:59 +0300

    [UG-88] Add participants list to Minigame class. Add participants interface to MinigamesManager class. Update config.

--- src/main/java/org/ultragore/everything/modules/MinigamesAdapter/managers/MinigamesManager.java
+++ src/main/java/org/ultragore/everything/modules/MinigamesAdapter/managers/MinigamesManager.java
@@ -6,8 +6,12 @@ import java.util.Map;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.ultragore.everything.modules.MinigamesAdapter.MinigamesAdapter;
 import org.ultragore.everything.modules.MinigamesAdapter.events.MinigameJoinEvent;
@@ -30,6 +34,7 @@ public class MinigamesManager implements Listener {
 	}
 	
 	
+	
 	public boolean hasMinigame(String worldName) {
 		for(Minigame m: minigames) {
 			if(m.worldName.equals(worldName)) {
@@ -48,6 +53,23 @@ public class MinigamesManager implements Listener {
 		return null;
 	}
 	
+	public Minigame getMinigame(Player participant) {
+		for(Minigame m: minigames) {
+			if(m.hasParticipant(participant)) {
+				return m;
+			}
+		}
+		return null;
+	}
+	
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent e) {
+		Minigame minigame = getMinigame(e.getPlayer());
+		if(minigame != null) {
+			minigame.removeParticipant(e.getPlayer());
+		}
+	}
 	
 	@EventHandler
 	public void onPlayerTeleport(PlayerTeleportEvent e) {
@@ -58,12 +80,27 @@ public class MinigamesManager implements Listener {
 		Minigame fromMinigame = getMinigame(e.getFrom().getWorld().getName());
 		Minigame toMinigame = getMinigame(e.getTo().getWorld().getName());
 		
+		if(fromMinigame != null && toMinigame != null && fromMinigame.worldName.equals(toMinigame.worldName)) {
+			return;
+		}
+		
 		if(fromMinigame != null) {
 			Bukkit.getPluginManager().callEvent(new MinigameLeaveEvent(e.getPlayer(), fromMinigame, lobbyManager.hasLobby(e.getTo().getWorld().getName())));
+			fromMinigame.removeParticipant(e.getPlayer());
 		}
 		
 		if(toMinigame != null) {
 			Bukkit.getPluginManager().callEvent(new MinigameJoinEvent(e.getPlayer(), toMinigame, lobbyManager.hasLobby(e.getFrom().getWorld().getName())));
+			toMinigame.addParticipant(e.getPlayer());
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerRespawn(PlayerRespawnEvent e) {
+		Minigame minigame = getMinigame(e.getPlayer());
+		
+		if(minigame != null) {
+			e.setRespawnLocation(minigame.deathRespawnLocation);
 		}
 	}
 }
--- src/main/java/org/ultragore/everything/modules/MinigamesAdapter/types/Minigame.java
+++ src/main/java/org/ultragore/everything/modules/MinigamesAdapter/types/Minigame.java
@@ -1,20 +1,40 @@
 package org.ultragore.everything.modules.MinigamesAdapter.types;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.ultragore.everything.utils.WorldUtils;
 
 public class Minigame {
 	public String gameGroup;
 	public String worldName;
+	public Location deathRespawnLocation;
+	private List<Player> participants = new ArrayList<Player>();
 	
 	public Minigame(Map<String, Object> map) {
 		gameGroup = (String) map.get("game_group");
 		worldName = (String) map.get("world_name");
+		deathRespawnLocation = WorldUtils.parseLocation((String) map.get("death_respawn_location"));
 	}
 	
 	public World getWorld() {
 		return Bukkit.getWorld(worldName);
+	}
+	
+	public boolean hasParticipant(Player participant) {
+		return participants.contains(participant);
+	}
+	
+	public void addParticipant(Player participant) {
+		participants.add(participant);
+	}
+	
+	public void removeParticipant(Player participant) {
+		participants.remove(participant);
 	}
 }
